261f9bf565bf08645d5560b063f631db3c913989
zhangwenwen12138 <39935062+zhangwenwen12138@users.noreply.github.com>
2018-08-30 09:28:07 +0800

    Update install.sh

---
+++ install.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+apkDir="/temp/apks"
+
+path=""
+
+subDir=""
+
+cd $apkDir
+
+function readDir(){
+
+   cd $apkDir
+
+   filelist=`ls $1`
+
+   for file in $filelist
+
+   do
+
+       installApk $file
+
+   done
+
+}
+
+function installApk(){
+
+   file=$1
+
+   extension="${file##*.}"
+
+   if [ "$extension" = "apk" ]
+
+   then
+
+       adb install $file
+
+   else
+
+       echo "Error:"$file "is not apk file."
+
+   fi
+
+}
+
+readDir $apkDir
